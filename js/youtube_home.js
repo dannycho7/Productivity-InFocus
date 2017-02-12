a35b8edde59570e85a25827f0557c42df6e8f57d
Danny H. Cho <dannycho7@gmail.com>
2017-02-11 23:27:20 -0800

    Youtube thumbnail changer addition

--- js/youtube_home.js
+++ js/youtube_home.js
@@ -1,5 +1,22 @@
-chrome.storage.sync.get('key',function(result){
+function thumbnail(){
+	chrome.storage.sync.get('key',function(result){
 		if(result.key == "true"){
-			//add the code to execute here.
+			console.log("running");
+			var thumbnail = document.getElementsByClassName('yt-thumb-simple');
+			if(thumbnail.length > 0){
+				changeImage(thumbnail);
+			}
+			var thumbVid = document.getElementsByClassName('yt-uix-simple-thumb-wrap');
+			if(thumbVid.length > 0){
+				changeImage(thumbVid);
+			}
+			function changeImage(thumb){
+				for(var i = 0; i < thumb.length; i++){
+					thumb[i].children[0].src = "https://i.imgflip.com/r2rlu.jpg";
+				}
+			}
 		}
-});
+	});
+}
+window.addEventListener('load',function(){thumbnail();});
+setInterval(thumbnail,3000);
