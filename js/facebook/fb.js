466335d216de883df822c03336a8f0dc87c7c597
Danny H. Cho <dannycho7@gmail.com>
2017-02-12 00:17:48 -0800

    added functionality to close and open modal for news feed

--- js/facebook/fb.js
+++ js/facebook/fb.js
@@ -4,39 +4,36 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log("URL CHANGED: " + request.data.url);
 });
 
-console.log("hell11o");
 function loaded(){
-	console.log("hello");
 	chrome.storage.sync.get('key',function(result){
 		function includeWarningMessage(){
-			console.log("hellero");
 			var topOfNewsFeed = document.querySelector('[id^="topnews_main_stream"]');
-      console.log("parent node is ", document.getElementById(topOfNewsFeed.id).parentNode);
 			if(topOfNewsFeed !== null) {
-        document.getElementById(topOfNewsFeed.id).style.display = "none";
-        var div = createModal();
-        console.log(div);
-        var contentArea = document.getElementById('stream_pagelet');
-        // var beforeTopOfNewsFeed = document.getElementById('pagelet_composer')
-  			// beforeTopOfNewsFeed.insertBefore(div, beforeTopOfNewsFeed.firstChild);
-        contentArea.insertBefore(div, document.getElementById(topOfNewsFeed.id));
-      }
-			// var node = document.getElementById('movie_player')
-			// node.insertBefore(div, node.firstChild);
+		        document.getElementById(topOfNewsFeed.id).style.display = "none";
+		        var div = createModal();
+		        console.log(div);
+		        var contentArea = document.getElementById('stream_pagelet');
+		        contentArea.insertBefore(div, document.getElementById(topOfNewsFeed.id));
+		    }
 		}
-
-    function createModal(){
+		function createModal(){
 			// creating the modal div
 			var div = document.createElement('div');
 			// setting div attributes
-			div.className = 'video-modal';
-      var facebookFeed = document.getElementById('pagelet_composer');
-      console.log("feed", facebookFeed);
-      var width = window.getComputedStyle(facebookFeed).getPropertyValue("width");
-      div.style.width = width;
+			div.className = 'modal';
+			var facebookFeed = document.getElementById('pagelet_composer');
+			var width = window.getComputedStyle(facebookFeed).getPropertyValue("width");
+			div.style.width = width;
 			// creating the button
 			var btn = document.createElement("button");
-			btn.addEventListener("click", closeModal);
+			function showContent(){
+				var modals = document.getElementsByClassName("modal");
+				for(var i = modals.length - 1; i >= 0; i--){
+					modals[i].remove();
+				}
+				document.querySelector('[id^="topnews_main_stream"]').style.display ="block";
+			}
+			btn.addEventListener("click", showContent);
 			// creating the header
 			var header = document.createElement("h1");
 			header.innerHTML = "Do you have time to scroll?";
@@ -44,10 +41,7 @@ function loaded(){
 			var description = document.createElement("p");
 			description.innerHTML = "Facebook feed will distract you from focusing!"
 			div.appendChild(description)
-			function closeModal() {
-				console.log(div);
-				div.parentNode.removeChild(div);
-			}
+			
 			var t = document.createTextNode("Proceed");
 			btn.appendChild(t);
 
@@ -62,7 +56,6 @@ function loaded(){
 				includeWarningMessage();
 		}
 		if(result.key == "true"){
-			console.log("reached result key=true");
 			hideNewsFeed();
 		}
 		else{
