d0b08fad90f1c00526e81a456fc2b00758186ae5
Danny H. Cho <dannycho7@gmail.com>
2017-02-11 16:42:31 -0800

    Cleaned up youtube_video.js code

--- js/youtube_video.js
+++ js/youtube_video.js
@@ -5,51 +5,18 @@ chrome.storage.sync.get('key',function(result){
 		}
 });
 
-//
-// document.addEventListener("spfdone", stopVideo);
-// document.addEventListener("DOMContentLoaded", stopVideo);
-// document.addEventListener("spfdone", hideContent);
-// document.addEventListener("DOMContentLoaded", hideContent);
-// var video = document.getElementsByClassName('player-api')[0];
-//
-// function pauseVid() {
-//     video.pause();
-// }
-// var myPlayer = document.getElementById('player');
-// function pauseVid() {
-//     myPlayer.pause();
-//     console.log("hi");
-// }
-// pauseVid();
-// myPlayer.stopVideo();
-
 function stopVideo() {
 	var iframe = document.querySelector('iframe');
 	var video = document.querySelector('video');
-  console.log("video", video);
-  console.log("iframe", iframe);
-	if (iframe){
-		var iframeSrc = iframe.src;
-		iframe.src = iframeSrc;
-	}
+	console.log("video", video);
 	if (video !== null) {
-    console.log("reached11");
+		console.log("reached11");
 		video.pause();
-    // fix it later.
 	}
 };
-//
-// document.querySelector('')
-//
-// stopVideo();
-// document.querySelector('video').pause();
-console.log("youtube_video.js");
+
 function hideContent(){
   document.getElementById("comment-section-renderer-items").style.visibility = "hidden";
-  // document.getElementById("comment-section-renderer-items").style.visibility = "hidden";
-  // if(content !== null){
-  //     content.style.visibility = "hidden";
-  // }
 }
 
 function includeWarningMessage(){
@@ -64,7 +31,3 @@ function includeWarningMessage(){
 			div.innerHTML = html;
 			document.getElementById('comment-section-renderer').appendChild(div);
 }
-setTimeout(hideContent, 10000);
-setTimeout(stopVideo, 10000);
-setTimeout(includeWarningMessage, 15000);
-// .style.visibility = "hidden";
