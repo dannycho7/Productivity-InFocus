226e39d57baa39b3922ee63f9cdbab675d9b80b6
Brian Yan <score14@gmail.com>
2017-02-11 18:31:09 -0800

    wip

--- js/youtube_video.js
+++ js/youtube_video.js
@@ -8,8 +8,10 @@ function loaded(){
 					console.log("Video found");
 					video.pause();
 					clearInterval(videostopper);
+					includeWarningMessage();
 				}
 			}
+
 			var videostopper = window.setInterval(function(){stopVideo();},1000); //interval for video stopping function
 		}
 		else{
@@ -21,18 +23,51 @@ function loaded(){
 function hideContent(){
 	document.getElementById("comment-section-renderer-items").style.visibility = "hidden";
 }
+function closeModal(){
+
+}
+
+function createModal(){
+	// creating the modal div
+	var html = [
+		'<div> A wild modal has appeared</div>'
+	].join('');
+	var div = document.createElement('div');
+	// setting div attributes
+	div.setAttribute('id', 'modal');
+	div.style.width = "100%";
+	div.style.height = "100%";
+	div.style.position = "absolute";
+	div.style.backgroundColor = "black";
+	div.style.color = "red";
+	div.style.fontSize = "40px";
+	div.style.zIndex = 11;
+	div.style.textAlign = "center";
+	div.innerHTML = html;
+	// creating the button
+	var btn = document.createElement("BUTTON");
+	btn.addEventListener("click", closeModal);
+
+
+	function closeModal() {
+		console.log(div);
+		div.parentNode.removeChild(div);
+	}
+	btn.style.zIndex = 12;
+	btn.style.color = "red";
+	var t = document.createTextNode("CLOSE ME");
+	btn.appendChild(t);
+
+	// ordering the structure of button and div
+	div.appendChild(btn);
+	return div;
+}
 
 function includeWarningMessage(){
-	var html = [
-		'<div> A line</div>',
-		'<div> Add more lines</div>',
-		'<div> To the array as you need.</div>'
-	].join('');
-
-	var div = document.createElement('div');
-		div.setAttribute('class', 'post block bc2');
-		div.innerHTML = html;
-		document.getElementById('comment-section-renderer').appendChild(div);
+	console.log("hello");
+	var div = createModal();
+	var node = document.getElementById('movie_player')
+	node.insertBefore(div, node.firstChild);
 }
 document.addEventListener("spfdone",loaded);
 document.addEventListener("DOMContentLoaded", loaded);
