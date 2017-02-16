2762d4992f21efebe2da20a183b0f6b3baa50d0e
Danny H. Cho <dannycho7@gmail.com>
2017-02-16 01:58:32 -0800

    Analytics page fixed for real time data

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,3 +1,28 @@
+function daysInMonth(month,year) {
+    return new Date(year, month, 0).getDate();
+}
+//num is a parameter that should be set to 0 if you want today's date
+//setting it higher will go back num number of days
+//assume that num is less than 15
+function formatToday(num){
+	var dateObj = new Date();
+	var month = dateObj.getUTCMonth() + 1; //months from 1-12
+	var day = dateObj.getUTCDate() - num;
+	var year = dateObj.getUTCFullYear();
+	if(day <= 0){
+		if(month == 1){
+			//you go back one year
+			month = 12;
+			year--;
+			day = daysInMonth(month,year); //sets the day to number of days in the previous month
+		}
+		else{
+			month--;
+			day += daysInMonth(month, year); //sets the day to number of days in the previous month
+		}
+	}
+	return "" + year + month + day;
+}
 function getStringDay(numday){
 	var weekday = ["Sunday","Monday","Tuesday","Wednesday", "Thursday","Friday","Saturday","Sunday"];
 	if(numday < 0){
@@ -6,9 +31,20 @@ function getStringDay(numday){
 	return weekday[numday];
 }
 function addChart(){
-	chrome.storage.sync.get(
-		['allTimeCount','videoCount','expiryTime'],
+	chrome.storage.sync.get(null,
 		function(result){
+			//returns the value at the key 
+			//returns 0 if it is not set
+			function parseData(key){
+				var data = result[key];
+				console.log(data);
+				if(data == undefined){
+					data = 0;
+				}
+				console.log(data);
+				return data;
+			}
+			console.log(formatToday(0));
 			var date = new Date();
 			console.log(getStringDay(date.getDay()));
 			var numday = date.getDay();
@@ -21,7 +57,7 @@ function addChart(){
 				labels: [getStringDay(numday-4), getStringDay(numday-3), getStringDay(numday-2), getStringDay(numday-1), getStringDay(numday)],
 				datasets: [{
 				label: '# of Videos watched',
-				data: [21,15,13,11,count],
+				data: [parseData(formatToday(4)),parseData(formatToday(3)), parseData(formatToday(2)), parseData(formatToday(1)),parseData(formatToday(0))],
 				backgroundColor: "rgba(218,127,127,0.4)",
 				borderColor: "rgba(138,28,28,0.5)",
 				pointBackgroundColor: "rgb(138,28,28)"
@@ -43,26 +79,26 @@ function addChart(){
 				labels: [getStringDay(numday-6), getStringDay(numday-5), getStringDay(numday-4), getStringDay(numday-3), getStringDay(numday-2), getStringDay(numday-1), getStringDay(numday)],
 				datasets: [
 				{
-				label: "Current week ",
-				fillColor : "rgba(220,220,220,0.5)",
-				strokeColor : "rgba(220,220,220,1)",
-				backgroundColor: "rgba(80, 244, 66, 0.4)",
-				pointBackgroundColor: "rgba(66, 244, 95, 0.8)",
-				borderColor: "rgba(80, 244, 66, 0.6)",
-				pointHoverBackgroundColor: "rgba(66, 244, 95, 0.8)",
-				pointHoverBorderColor: "rgba(255, 255, 255, 1)",
-				data: [65, 59, 90, 81, 56, 55, count]
+					label: "Current week ",
+					fillColor : "rgba(220,220,220,0.5)",
+					strokeColor : "rgba(220,220,220,1)",
+					backgroundColor: "rgba(80, 244, 66, 0.4)",
+					pointBackgroundColor: "rgba(66, 244, 95, 0.8)",
+					borderColor: "rgba(80, 244, 66, 0.6)",
+					pointHoverBackgroundColor: "rgba(66, 244, 95, 0.8)",
+					pointHoverBorderColor: "rgba(255, 255, 255, 1)",
+					data: [parseData(formatToday(6)), parseData(formatToday(5)), parseData(formatToday(4)), parseData(formatToday(3)), parseData(formatToday(2)), parseData(formatToday(1)), parseData(formatToday(0))]
 				},
 				{
-				label: "Last week",
-				fillColor : "rgba(151,187,205,0.5)",
-				strokeColor : "rgba(151,187,205,1)",
-				backgroundColor: "rgba(66, 122, 244 , 0.4)",
-				pointBackgroundColor: "rgba(66, 134, 244, 0.8)",
-				borderColor: "rgba(66, 122, 244 , 0.6)",
-				pointHoverBackgroundColor: "rgba(66, 134, 244, 0.8)",
-				pointHoverBorderColor: "rgba(255, 255, 255, 1)",
-				data: [28, 48, 40, 19, 96, 27, 60]
+					label: "Last week",
+					fillColor : "rgba(151,187,205,0.5)",
+					strokeColor : "rgba(151,187,205,1)",
+					backgroundColor: "rgba(66, 122, 244 , 0.4)",
+					pointBackgroundColor: "rgba(66, 134, 244, 0.8)",
+					borderColor: "rgba(66, 122, 244 , 0.6)",
+					pointHoverBackgroundColor: "rgba(66, 134, 244, 0.8)",
+					pointHoverBorderColor: "rgba(255, 255, 255, 1)",
+					data: [parseData(formatToday(13)), parseData(formatToday(12)), parseData(formatToday(11)), parseData(formatToday(10)), parseData(formatToday(9)), parseData(formatToday(8)), parseData(formatToday(7))]
 				}
 				]
 			}; // end data
---
+++ src/js/background.js
@@ -0,0 +1,44 @@
+function daysInMonth(month,year) {
+    return new Date(year, month, 0).getDate();
+}
+//num is a parameter that should be set to 0 if you want today's date
+//setting it higher will go back num number of days
+//assume that num is less than 15
+function formatToday(num){
+	var dateObj = new Date();
+	var month = dateObj.getUTCMonth() + 1; //months from 1-12
+	var day = dateObj.getUTCDate() - num;
+	var year = dateObj.getUTCFullYear();
+	if(day <= 0){
+		if(month == 1){
+			//you go back one year
+			month = 12;
+			year--;
+			day = daysInMonth(month,year); //sets the day to number of days in the previous month
+		}
+		else{
+			month--;
+			day += daysInMonth(month, year); //sets the day to number of days in the previous month
+		}
+	}
+	return "" + year + month + day;
+}
+//Background script to change the icon based on user settings
+chrome.storage.sync.get(null,function(result){
+	if(result.key == "true"){
+		chrome.browserAction.setIcon({path:"/img/on.png"});
+	}
+	else{
+		chrome.browserAction.setIcon({path:"/img/off.png"});
+	}
+	if(result.allTimeCount == undefined){
+		chrome.storage.sync.set({'allTimeCount':'0',},function(){ console.log("setallTimeCount"); })
+	}
+	//setting default videoCount
+	if(result[formatToday(0)] == undefined){
+		var obj = {};
+		obj[formatToday(0)] = 0;
+		console.log(obj[formatToday(0)]);
+		chrome.storage.sync.set(obj,function(){ console.log("set vidCount and expiryTime"); });
+	}
+});
